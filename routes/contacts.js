98854911748af155b53ded656cddf6cfc57561c9
Zach Smith <zsmith126028@gmail.com>
2019-06-08 18:51:34 -0500

    add route to get user contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,31 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 // @route GET api/contacts
 // @desc Get all users' contacts
 // @access Private
-router.get("/", (req, res) => {
-  res.send("Get all contacts");
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route POST api/contacts
 // @desc Add new contact
 // @access Private
-router.post("/", (req, res) => {
+router.post("/", auth, (req, res) => {
   res.send("Add new contact");
 });
 
 // @route PUT api/contacts/:id
 // @desc Update a contact
-// @access Prvi==ivate
-router.put("/:id", (req, res) => {
+// @access Private
+router.put("/:id", auth, (req, res) => {
   res.send("Update contact");
 });
 
 // @route DELETE api/users/:id
 // @desc Delete a user
 // @access Private
-router.delete("/:id", (req, res) => {
+router.delete("/:id", auth, (req, res) => {
   res.send("Delete a contact");
 });
 
