bea98d24d6071f11e192bab28ac75ae4b68b0217
Zach Smith <zsmith126028@gmail.com>
2019-06-14 15:18:21 -0500

    add error to contact state

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -17,7 +17,8 @@ const ContactState = props => {
   const initialState = {
     contacts: [],
     current: null,
-    filtered: null
+    filtered: null,
+    error: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -78,13 +79,15 @@ const ContactState = props => {
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
         filterContacts,
-        clearFilter
+        clearFilter,
+        
       }}
     >
       {props.children}
