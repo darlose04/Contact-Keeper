b2bf8ea3fb4df881ba62149081e1a70551392e8e
Zach Smith <zsmith126028@gmail.com>
2019-06-14 15:12:36 -0500

    remove hard coded contacts, add connection to backend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,42 +1,21 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
   ADD_CONTACT,
   DELETE_CONTACT,
+  CONTACT_ERROR,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER
 } from "../types";
+import axios from "axios";
 
 const ContactState = props => {
   const initialState = {
-    contacts: [
-      {
-        id: 1,
-        name: "Jill Johnson",
-        email: "jill@gmail.com",
-        phone: "111-111-1111",
-        type: "personal"
-      },
-      {
-        id: 2,
-        name: "Sara Watson",
-        email: "sara@gmail.com",
-        phone: "222-222-2222",
-        type: "personal"
-      },
-      {
-        id: 3,
-        name: "Harry White",
-        email: "harry@gmail.com",
-        phone: "333-333-3333",
-        type: "professional"
-      }
-    ],
+    contacts: [],
     current: null,
     filtered: null
   };
@@ -45,8 +24,22 @@ const ContactState = props => {
 
   // Add contact
   const addContact = contact => {
-    contact.id = uuid.v4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post('/api/contacts', contact, config);
+
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ 
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      })
+    }
   };
 
   // Delete contact
