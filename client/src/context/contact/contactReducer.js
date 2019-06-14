13e12ad2fd152bf94435fb1ebdbc591fd521e40a
Zach Smith <zsmith126028@gmail.com>
2019-06-14 15:28:14 -0500

    add case for GET_CONTACTS, add loading: false to other cases

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -6,22 +6,31 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
-  CONTACT_ERROR
+  CONTACT_ERROR,
+  GET_CONTACTS
 } from "../types";
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false
+      };
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload]
+        contacts: [...state.contacts, action.payload],
+        loading: false
       };
     case UPDATE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.map(contact =>
           contact.id === action.payload.id ? action.payload : contact
-        )
+        ),
+        loading: false
       };
     case DELETE_CONTACT:
       return {
@@ -29,7 +38,8 @@ export default (state, action) => {
         // this will return all contacts that are not the current id, which is the one being removed
         contacts: state.contacts.filter(
           contact => contact.id !== action.payload
-        )
+        ),
+        loading: false
       };
     case SET_CURRENT:
       return {
