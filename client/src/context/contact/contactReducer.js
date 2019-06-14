13687c2f2ca31aa29010c5da86a36ac3076dd54a
Zach Smith <zsmith126028@gmail.com>
2019-06-14 15:43:05 -0500

    add case for CLEAR_CONTACTS

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -7,7 +7,8 @@ import {
   FILTER_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR,
-  GET_CONTACTS
+  GET_CONTACTS,
+  CLEAR_CONTACTS
 } from "../types";
 
 export default (state, action) => {
@@ -40,6 +41,14 @@ export default (state, action) => {
           contact => contact.id !== action.payload
         ),
         loading: false
+      };
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filtered: null,
+        error: null,
+        current: null
       };
     case SET_CURRENT:
       return {
